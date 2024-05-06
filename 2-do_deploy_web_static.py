305cd30ad25e499e9e6e4235b5984e03277d8e8c
ilyassdaaif <ilyass.daaif@gmail.com>
2024-05-05 18:16:23 -0700

    origin

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,44 +1,30 @@
 #!/usr/bin/python3
-"""distributes an archive to your web servers, using the function do_deploy:"""
-from fabric.api import env, run, put, local, sudo
-from os.path import isfile
+"""
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
+"""
 
-# Define the servers to deploy to
-env.hosts = ['54.152.5.96', '100.25.4.103']  # Replace with the actual IPs or hostnames
-env.user = 'ubuntu'  # Replace with your SSH user
-env.key = '/path/to/my_ssh_private_key'  # Replace with your actual SSH private key path
+from fabric.api import put, run, env
+from os.path import exists
+env.hosts = ['54.89.109.87', '100.25.190.21']
+
 
 def do_deploy(archive_path):
-    # Check if the archive exists locally
-    if not isfile(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
-
     try:
-        # Extract the file name and the base directory name from the archive path
-        file_name = archive_path.split('/')[-1]
-        base_dir = file_name.replace('.tgz', '').replace('.gz', '')
-
-        # Upload the archive to the /tmp/ directory on the web server
-        remote_tmp_path = f"/tmp/{file_name}"
-        put(archive_path, remote_tmp_path)
-
-        # Create the target directory where we will uncompress the archive
-        release_dir = f"/data/web_static/releases/{base_dir}/"
-        run(f"mkdir -p {release_dir}")
-
-        # Uncompress the archive to the folder on the web server and then delete the archive
-        run(f"tar -xzf {remote_tmp_path} -C {release_dir}")
-        run(f"rm {remote_tmp_path}")
-
-        # Move contents out of the web_static folder to the base directory
-        run(f"mv {release_dir}web_static/* {release_dir}")
-        run(f"rm -rf {release_dir}web_static")
-
-        # Delete the current symbolic link and create a new one
-        sudo("rm -rf /data/web_static/current")
-        sudo(f"ln -s {release_dir} /data/web_static/current")
-
-        print("New version deployed!")
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
     except:
         return False
