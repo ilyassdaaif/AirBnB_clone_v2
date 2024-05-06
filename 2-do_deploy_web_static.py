756598a58585fb8010a350d27f546d9ee2e6723b
ilyassdaaif <94968783+ilyassdaaif@users.noreply.github.com>
2024-05-06 01:32:35 +0100

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,60 +1,41 @@
 #!/usr/bin/python3
-"""A module for web application deployment with Fabric."""
-import os
-from datetime import datetime
-from fabric.api import env, local, put, run, runs_once
+"""distributes an archive to your web servers, using the function do_deploy:"""
+from fabric.api import env, run, put, local
+from os.path import exists
 
-
-env.hosts = ["34.73.0.174", "35.196.78.105"]
-"""The list of host server IP addresses."""
-
-
-@runs_once
-def do_pack():
-    """Archives the static files."""
-    if not os.path.isdir("versions"):
-        os.mkdir("versions")
-    cur_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        cur_time.year,
-        cur_time.month,
-        cur_time.day,
-        cur_time.hour,
-        cur_time.minute,
-        cur_time.second
-    )
-    try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
-        archize_size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
-    except Exception:
-        output = None
-    return output
-
+env.hosts = ['xx-web-01', 'xx-web-02']  # Replace with actual IPs
+env.user = "ubuntu"  # Set as needed, or define in command line
+env.key_filename = "my_ssh_private_key"  # Set path to your SSH key, or define in command line
 
 def do_deploy(archive_path):
-    """Deploys the static files to the host servers.
-    Args:
-        archive_path (str): The path to the archived static files.
-    """
-    if not os.path.exists(archive_path):
+    # Check if the archive_path exists locally
+    if not exists(archive_path):
         return False
-    file_name = os.path.basename(archive_path)
-    folder_name = file_name.replace(".tgz", "")
-    folder_path = "/data/web_static/releases/{}/".format(folder_name)
-    success = False
-    try:
-        put(archive_path, "/tmp/{}".format(file_name))
-        run("mkdir -p {}".format(folder_path))
-        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
-        run("rm -rf /tmp/{}".format(file_name))
-        run("mv {}web_static/* {}".format(folder_path, folder_path))
-        run("rm -rf {}web_static".format(folder_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(folder_path))
-        print('New version deployed!')
-        success = True
-    except Exception:
-        success = False
-    return success
+
+    # Extract the file name from the archive_path
+    file_name = archive_path.split("/")[-1]
+    # Remove the extension to get the base directory name
+    base_dir = file_name.split(".")[0]
+
+    # Upload the archive to the /tmp/ directory on the server
+    remote_path = f"/tmp/{file_name}"
+    put(archive_path, remote_path)
+
+    # Create the directory where we will uncompress the archive
+    release_dir = f"/data/web_static/releases/{base_dir}/"
+    run(f"mkdir -p {release_dir}")
+
+    # Uncompress the archive to the folder on the web server and then delete the archive
+    run(f"tar -xzf {remote_path} -C {release_dir}")
+    run(f"rm {remote_path}")
+
+    # Move contents out of the web_static folder to the base directory and remove web_static directory
+    run(f"mv {release_dir}web_static/* {release_dir}")
+    run(f"rm -rf {release_dir}web_static")
+
+    # Delete the current symbolic link and create a new one
+    run("rm -rf /data/web_static/current")
+    run(f"ln -s {release_dir} /data/web_static/current")
+
+    print("New version deployed!")
+    return True
