fb14dd277d08a082f2f5c3dd72d499c12f8df1b6
ilyassdaaif <94968783+ilyassdaaif@users.noreply.github.com>
2024-05-07 01:41:48 +0100

    Update 100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,96 +1,36 @@
 #!/usr/bin/python3
 """
-that deletes out-of-date archives,
-using the function do_clean
+Fabric script to clean up old archives of web static files from deployment.
+This script keeps only a specified number of the most recent archives both locally and on remote servers.
 """
-from datetime import datetime
-from fabric.api import local, put, run, env
-import os.path
 
-env.hosts = ['35.196.31.36', '35.237.103.48']
+from fabric.api import *
+from fabric.contrib import files
+import os
 
-
-def do_pack():
-    """
-    generate tgz archive using fabric
-    """
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    file_path = "versions/web_static_{}.tgz".format(date)
-    if os.path.isdir("versions") is False:
-        local(" mkdir versions")
-    local('tar -cvzf ' + file_path + ' web_static')
-    if os.path.exists(file_path):
-        return file_path
-    return None
-
-
-def do_deploy(archive_path):
-    """
-        deploy archive to web servers
-    """
-    if os.path.exists(archive_path) is False:
-        return False
-    arch_name = archive_path.split('/')[1]
-    arch_name_nex = arch_name.split(".")[0]
-    re_path = "/data/web_static/releases/" + arch_name_nex
-    up_path = '/tmp/' + arch_name
-    put(archive_path, up_path)
-    run('mkdir -p ' + re_path)
-    run('tar -xzf /tmp/{} -C {}/'.format(arch_name, re_path))
-    run('rm {}'.format(up_path))
-    mv = 'mv ' + re_path + '/web_static/* ' + re_path + '/'
-    run(mv)
-    run('rm -rf ' + re_path + '/web_static')
-    run('rm -rf /data/web_static/current')
-    run('ln -s ' + re_path + ' /data/web_static/current')
-    return True
-
-
-def deploy():
-    """
-    The script should take the following steps:
-
-    Call the do_pack() function and store the path of the created archive
-    Return False if no archive has been created
-    Call the do_deploy(archive_path) function,
-    using the new path of the new archive
-    Return the return value of do_deploy
-    """
-    path = do_pack()
-    if (path is None):
-        return False
-    deploy = do_deploy(path)
-    if (deploy is False):
-        return False
-    return deploy
-
+# Define the default user, host IPs and the SSH key location
+env.user = 'ubuntu'
+env.hosts = ['IP_web-01', 'IP_web-02']  # Replace with actual IPs of the servers
+env.key_filename = 'path_to_my_ssh_private_key'  # Optional, depends on your setup
 
 def do_clean(number=0):
     """
-    clean arch
+    Cleans up old archives. By default, or if number is 0 or 1, it keeps only the most recent archive.
+    If number is greater than 1, it will keep the specified number of the most recent archives.
     """
-    try:
-        number = int(number)
-    except Exception:
-        return False
-    nb_of_arch = local('ls -ltr versions | wc -l', capture=True).stdout
-    nb_of_arch = int(nb_of_arch) - 1
-    if nb_of_arch <= 0 or nb_of_arch == 1:
-        return True
-    if number == 0 or number == 1:
-        arch_to_rm = nb_of_arch - 1
-    else:
-        arch_to_rm = arch_to_rm - number
-        if arch_to_rm <= 0:
-            return True
-    archives = local("ls -ltr versions | tail -n " + str(nb_of_arch) + "\
-            | head -n \
-            " + str(arch_to_rm) + "\
-            | awk '{print $9}'", capture=True)
-    archives_list = archives.rsplit('\n')
-    if len(archives_list) >= 1:
-        for arch in archives_list:
-            if (arch != ''):
-                local("rm versions/" + arch)
-                run('rm -rf /data/web_static/releases/\
-                    ' + arch.split('.')[0])
+    number = int(number)
+    if number == 0:
+        number = 1
+
+    # Clean up local archives in the 'versions' directory
+    local('ls -t versions/*.tgz | tail -n +{} | xargs rm -rf'.format(number + 1))
+
+    # Clean up archives in the '/data/web_static/releases' directory on remote servers
+    with cd('/data/web_static/releases'):
+        run('ls -t | grep "web_static_" | tail -n +{} | xargs rm -rf'.format(number + 1))
+
+    # Check if the current symlink is intact; warn if it appears to be broken
+    if not files.exists('/data/web_static/current'):
+        print("Warning: The current symlink is broken. You may need to redeploy the static content.")
+
+# Additional deployment tasks can be defined here as needed
