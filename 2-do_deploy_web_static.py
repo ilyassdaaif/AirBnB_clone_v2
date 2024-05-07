94331ee9ad801abf6ce4c4e04dadd2b3ab448fcf
ilyassdaaif <94968783+ilyassdaaif@users.noreply.github.com>
2024-05-07 02:21:13 +0100

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,41 +1,52 @@
 #!/usr/bin/python3
 """
-Fabric script (based on the file 1-pack_web_static.py)
-that distributes an archive to your web servers, using the function do_deploy
+Fabric script that distributes an archive to web servers,
+extracts it, and sets up a new web static folder structure.
 """
-from os import path
-from fabric.api import env, put, run
 
-env.hosts = ["54.152.5.96", "100.25.4.103"]
+from fabric.api import *
+from os.path import exists
 
+env.user = 'ubuntu'
+env.hosts = ['54.152.5.96', '100.25.4.103']  # Replace with actual IPs
+env.key_filename = 'path_to_my_ssh_private_key'  # Path to SSH private key
 
 def do_deploy(archive_path):
     """
-    Distributes archives to web servers
+    Deploys the archive to web servers.
+    :param archive_path: Path to the archive to be deployed
+    :return: Boolean, True if all operations are done correctly, otherwise False
     """
-    if not path.exists(archive_path):
+    if not exists(archive_path):
         return False
-    compressedFile = archive_path.split("/")[-1]
-    fileName = compressedFile.split(".")[0]
-    upload_path = "/tmp/{}".format(compressedFile)
-    if put(archive_path, upload_path).failed:
+
+    try:
+        # Upload the archive to the /tmp/ directory on the web server
+        put(archive_path, '/tmp/')
+
+        # Extract the archive filename without the extension
+        file_name = archive_path.split("/")[-1]
+        folder_name = file_name.split(".")[0]
+
+        # Define the full remote path to the archive and release directory
+        remote_path = "/data/web_static/releases/" + folder_name + '/'
+
+        # Prepare release folder and unarchive the file there
+        run("mkdir -p {}".format(remote_path))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, remote_path))
+
+        # Remove the archive from the server after unarchiving
+        run("rm /tmp/{}".format(file_name))
+
+        # Move content from web_static subfolder to the parent folder
+        run("mv {0}web_static/* {0}".format(remote_path))
+        run("rm -rf {}web_static".format(remote_path))
+
+        # Delete the current symbolic link and create a new one
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(remote_path))
+
+        print("New version deployed!")
+        return True
+    except:
         return False
-    current_release = '/data/web_static/releases/{}'.format(fileName)
-    if run("rm -rf {}".format(current_release)).failed:
-        return False
-    if run("mkdir -p {}".format(current_release)).failed:
-        return False
-    uncompress = "tar -xzf /tmp/{} -C {}".format(
-        compressedFile, current_release
-    )
-    if run(uncompress).failed:
-        return False
-    delete_archive = "rm -f /tmp/{}".format(compressedFile)
-    if run(delete_archive).failed:
-        return False
-    if run("rm -rf /data/web_static/current").failed:
-        return False
-    relink = "ln -s {} /data/web_static/current".format(current_release)
-    if run(relink).failed:
-        return False
-    return True
