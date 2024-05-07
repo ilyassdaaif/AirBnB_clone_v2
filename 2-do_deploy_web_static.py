6601f00fe6205fdf5cadb33f16a1a3d9c706f331
ilyassdaaif <94968783+ilyassdaaif@users.noreply.github.com>
2024-05-07 02:44:12 +0100

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,52 +1,30 @@
 #!/usr/bin/python3
 """
-Fabric script that distributes an archive to web servers,
-extracts it, and sets up a new web static folder structure.
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
 """
 
-from fabric.api import *
+from fabric.api import put, run, env
 from os.path import exists
+env.hosts = ['54.152.5.96', '100.25.4.103']
 
-env.user = 'ubuntu'
-env.hosts = ['54.152.5.96', '100.25.4.103']  # Replace with actual IPs
-env.key_filename = 'path_to_my_ssh_private_key'  # Path to SSH private key
 
 def do_deploy(archive_path):
-    """
-    Deploys the archive to web servers.
-    :param archive_path: Path to the archive to be deployed
-    :return: Boolean, True if all operations are done correctly, otherwise False
-    """
-    if not exists(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
-
     try:
-        # Upload the archive to the /tmp/ directory on the web server
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-
-        # Extract the archive filename without the extension
-        file_name = archive_path.split("/")[-1]
-        folder_name = file_name.split(".")[0]
-
-        # Define the full remote path to the archive and release directory
-        remote_path = "/data/web_static/releases/" + folder_name + '/'
-
-        # Prepare release folder and unarchive the file there
-        run("mkdir -p {}".format(remote_path))
-        run("tar -xzf /tmp/{} -C {}".format(file_name, remote_path))
-
-        # Remove the archive from the server after unarchiving
-        run("rm /tmp/{}".format(file_name))
-
-        # Move content from web_static subfolder to the parent folder
-        run("mv {0}web_static/* {0}".format(remote_path))
-        run("rm -rf {}web_static".format(remote_path))
-
-        # Delete the current symbolic link and create a new one
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(remote_path))
-
-        print("New version deployed!")
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
     except:
         return False
