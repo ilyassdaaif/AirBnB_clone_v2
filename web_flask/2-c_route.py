9d9a005140095e5353f44aabe180c69a7faa6935
ilyassdaaif <ilyassdaaif@gmail.com>
2024-05-17 11:56:06 -0700

    origin

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,21 +1,32 @@
 #!/usr/bin/python3
-""" Starts a Flask web application """
+"""Starts a Flask web application.
+"""
 
 from flask import Flask
 
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    return "Hello HBNB!"
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    return "HBNB"
+@app.route('/')
+def hello_world():
+    """ Returns some text. """
+    return 'Hello HBNB!'
 
-@app.route('/c/<text>', strict_slashes=False)
+
+@app.route('/hbnb')
+def hello():
+    """ Return other text. """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
 def c_text(text):
-    return "C " + text.replace('_', ' ')
+    """ replace text with variable. """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
