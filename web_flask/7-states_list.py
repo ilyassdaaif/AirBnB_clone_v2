1258a46c9ed1db3ae6f6fcb05af1362e6dc237b0
ilyassdaaif <94968783+ilyassdaaif@users.noreply.github.com>
2024-05-18 20:14:39 +0100

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
+"""Starts a Flask web application to display a list of states"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+
 app = Flask(__name__)
-
-
-@app.teardown_appcontext
-def teardown_db(_):
-    """Close storage."""
-    storage.close()
-
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Display a HTML page."""
-    states = storage.all(State)
+    """Displays a HTML page with a list of states"""
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda state: state.name)
     return render_template('7-states_list.html', states=states)
 
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Closes the storage on teardown"""
+    storage.close()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
